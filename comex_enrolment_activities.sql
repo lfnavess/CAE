c06f01b2d0c03d52a3c43cc9f8c748428dc0ff63
Luis Naves <lfnavess@gmail.com>
2017-10-16 08:21:04 -0500

    Add files via upload

--- comex_enrolment_activities.sql
+++ comex_enrolment_activities.sql
@@ -1,4 +1,4 @@
-IF OBJECT_ID('dbo.comex_enrolment_activities', 'V') IS NOT NULL DROP VIEW dbo.comex_enrolment_activities;
+IF OBJECT_ID('dbo.comex_enrolment_activities', 'V')IS NOT NULL DROP VIEW dbo.comex_enrolment_activities;
 GO
 CREATE VIEW dbo.comex_enrolment_activities
 AS
@@ -11,19 +11,18 @@ SELECT
         ELSE 0
     END                                                                                             AS"progress_measure",
     CASE
-        WHEN "vt_activities_lang"."title" = 'Autodiagnóstico'                                           THEN 1
-        WHEN "vt_ca_access_control"."scoreweight" IS NULL OR "vt_ca_access_control"."scoreweight" <>    THEN 2
+        WHEN"vt_activities_lang"."title"='Autodiagnóstico'                                          THEN 1
+        WHEN"vt_ca_access_control"."scoreweight"IS NULL OR"vt_ca_access_control"."scoreweight"<>    THEN 2
     END                                                                                             AS"score_type",
     CASE
         WHEN
-            "vt_courses"."minscore" IS NOT NULL
+            "vt_courses"."minscore"IS NOT NULL
             AND"vt_enrolment_activities"."cmi_completion_status"='completed'
-            AND"vt_enrolment_activities"."cmi_score_max" > 0
+            AND"vt_enrolment_activities"."cmi_score_max">0
         THEN
             CAST(
-                CAST("vt_enrolment_activities"."cmi_score_raw" - "vt_enrolment_activities"."cmi_score_min" AS DECIMAL(7,4))
-                / CAST("vt_enrolment_activities"."cmi_score_max" - "vt_enrolment_activities"."cmi_score_min" AS DECIMAL(7,4))
-                *100
+                CAST("vt_enrolment_activities"."cmi_score_raw"-"vt_enrolment_activities"."cmi_score_min"AS DECIMAL(7,4))
+                /CAST("vt_enrolment_activities"."cmi_score_max"-"vt_enrolment_activities"."cmi_score_min"AS DECIMAL(7,4))*100
                 AS INT
             )
     END                                                                                             AS"score",
@@ -31,10 +30,13 @@ SELECT
     "vt_enrolment_activities"."cmi_suspend_data"
 FROM
     "vt_enrolments"
-    LEFT JOIN"vt_course_groups"         ON"vt_course_groups"."id" = "vt_enrolments"."groupid"
-    LEFT JOIN"vt_courses"               ON"vt_courses"."id" = "vt_course_groups"."courseid"
-    LEFT JOIN"vt_course_activities"     ON"vt_course_activities"."courseid" = "vt_courses"."id"
-    JOIN"vt_activities"                 ON"vt_activities"."type" = 1 AND"vt_activities"."id"="vt_course_activities"."activityid"
-    LEFT JOIN"vt_ca_access_control"     ON"vt_ca_access_control"."groupid" = "vt_course_groups"."id"AND"vt_ca_access_control"."activityid"="vt_course_activities"."id"
+    LEFT JOIN"vt_course_groups"         ON"vt_course_groups"."id"="vt_enrolments"."groupid"
+    LEFT JOIN"vt_courses"               ON"vt_courses"."id"="vt_course_groups"."courseid"
+    LEFT JOIN"vt_course_activities"     ON"vt_course_activities"."courseid"="vt_courses"."id"
+    JOIN"vt_activities"                 ON"vt_activities"."type"=1 AND"vt_activities"."id"="vt_course_activities"."activityid"
+    LEFT JOIN"vt_ca_access_control"     ON"vt_ca_access_control"."groupid"="vt_course_groups"."id"
+        AND"vt_ca_access_control"."activityid"="vt_course_activities"."id"
     LEFT JOIN"vt_activities_lang"       ON"vt_activities_lang"."lang"='xx'AND"vt_activities_lang"."activityid" = "vt_course_activities"."activityid"
-    LEFT JOIN"vt_enrolment_activities"  ON"vt_enrolment_activities"."enrolmentid"="vt_enrolments"."id"AND"vt_enrolment_activities"."activityid"="vt_course_activities"."id";
+    LEFT JOIN"vt_enrolment_activities"  ON
+        "vt_enrolment_activities"."enrolmentid"="vt_enrolments"."id"
+        AND"vt_enrolment_activities"."activityid"="vt_course_activities"."id";
